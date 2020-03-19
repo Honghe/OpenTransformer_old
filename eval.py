6bb74cab9bc77d8063a78c3dddcf1256aaac4828
Jack Wu <leopardsaga@gmail.com>
2020-03-19 11:38:53 +0800

    Add CER metric.

--- eval.py
+++ eval.py
@@ -1,14 +1,44 @@
-import os
-import yaml
-import torch
 import argparse
+import os
+
+import torch
+import yaml
+
+from otrans.data import load_vocab, AudioDataset, FeatureLoader
 from otrans.model import Transformer
 from otrans.recognizer import TransformerRecognizer
-from otrans.data import load_vocab, AudioDataset, FeatureLoader
+
+
+def calc_cer(r: list, h: list):
+    """
+    Calculation of CER with Levenshtein distance.
+    """
+    # initialisation
+    import numpy
+    d = numpy.zeros((len(r) + 1) * (len(h) + 1), dtype=numpy.uint16)
+    d = d.reshape((len(r) + 1, len(h) + 1))
+    for i in range(len(r) + 1):
+        for j in range(len(h) + 1):
+            if i == 0:
+                d[0][j] = j
+            elif j == 0:
+                d[i][0] = i
+
+    # computation
+    for i in range(1, len(r) + 1):
+        for j in range(1, len(h) + 1):
+            if r[i - 1] == h[j - 1]:
+                d[i][j] = d[i - 1][j - 1]
+            else:
+                substitution = d[i - 1][j - 1] + 1
+                insertion = d[i][j - 1] + 1
+                deletion = d[i - 1][j] + 1
+                d[i][j] = min(substitution, insertion, deletion)
+
+    return d[len(r)][len(h)] / float(len(r))
 
 
 def main(args):
-
     checkpoint = torch.load(args.load_model)
     if 'params' in checkpoint:
         params = checkpoint['params']
@@ -41,17 +71,18 @@ def main(args):
         model.cuda()
 
     char2unit = load_vocab(params['data']['vocab'])
-    unit2char = {i:c for c, i in char2unit.items()}
+    unit2char = {i: c for c, i in char2unit.items()}
 
     dataset = AudioDataset(params['data'], args.decode_set)
     data_loader = FeatureLoader(dataset)
-    
+
     recognizer = TransformerRecognizer(model, unit2char=unit2char, beam_width=args.beam_width,
                                        max_len=args.max_len, penalty=args.penalty, lamda=args.lamda, ngpu=args.ngpu)
 
     totals = len(dataset)
     batch_size = params['data']['batch_size']
     writer = open(os.path.join(decode_dir, 'predict.txt'), 'w')
+    cers = []
     for step, (utt_id, batch) in enumerate(data_loader.loader):
 
         if args.ngpu > 0:
@@ -65,11 +96,14 @@ def main(args):
 
         for b in range(len(preds)):
             n = step * batch_size + b
-            truth = ' '.join([unit2char[i.item()] for i in targets[b][1:targets_length[b]]])
-            print('[%d / %d ] %s - pred : %s' % (n, totals, utt_id[b], preds[b]))
-            print('[%d / %d ] %s - truth: %s' % (n, totals, utt_id[b], truth))
+            truth = ' '.join([unit2char[i.item()] for i in targets[b][1:targets_length[b] + 1]])
+            print('[%d / %d] %s - pred : %s' % (n, totals, utt_id[b], preds[b]))
+            print('[%d / %d] %s - truth: %s' % (n, totals, utt_id[b], truth))
+            cer = calc_cer(truth.split(), preds[b].split())
+            cers.append(cer)
+            print('cer: {:.2f}'.format(cer))
             writer.write(utt_id[b] + ' ' + preds[b] + '\n')
-
+    print('cer avg: {:.2f}'.format(sum(cers) / len(cers)))
     writer.close()
 
 
@@ -81,7 +115,7 @@ if __name__ == '__main__':
     parser.add_argument('-bw', '--beam_width', type=int, default=5)
     parser.add_argument('-p', '--penalty', type=float, default=0.6)
     parser.add_argument('-ld', '--lamda', type=float, default=5)
-    parser.add_argument('-m','--load_model', type=str, default=None)
+    parser.add_argument('-m', '--load_model', type=str, default=None)
     parser.add_argument('-d', '--decode_set', type=str, default='test')
     parser.add_argument('-ml', '--max_len', type=int, default=100)
     parser.add_argument('-s', '--suffix', type=str, default=None)
